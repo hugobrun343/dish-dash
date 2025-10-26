b184d0760b606eed8d2c5018bd9dd3867f25e1bf
hugobrun <hugo.brun@etu.umontpellier.fr>
2025-10-26 21:49:55 +0100

    Add NEXT_PUBLIC_API_URL validation in Dockerfile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     domains: ['localhost', 'images.unsplash.com'],
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/only/for/test/build/success',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 }
 
